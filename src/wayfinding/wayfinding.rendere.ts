4a75e472bbb4d12d149a239dd2210d36172b698a
Rasmus Rune Andersen <rasmus@entropyfox.com>
2022-03-31 16:13:22 +0200

    Moar paaaarticles!

--- src/wayfinding/wayfinding.rendere.ts
+++ src/wayfinding/wayfinding.rendere.ts
@@ -27,45 +27,39 @@ export const WayfindingRendere = async (canvas: HTMLCanvasElement) => {
         // Load Particle system
         // We don't have to load the particles in this callback
         loadParticles: {
-            const emitterBox = MeshBuilder.CreateBox("emitter", {size:3})
-            emitterBox.position = new Vector3(1, 0, 8)
-            emitterBox.setPivotPoint(new Vector3(0,0,0))
-            emitterBox.visibility = 0.5
+            const emitterBox = MeshBuilder.CreateBox("emitter", {size:2})
+            emitterBox.position = new Vector3(2, 0, 0)
+            emitterBox.setPivotPoint(new Vector3(-2,0,0))
+            emitterBox.visibility = 0.4
 
             // Create a particle system
             const particleSystem = new ParticleSystem("particles", 2000, scene);
-            
-
-            
 
             //Particle emitter
             //particleSystem.createBoxEmitter(new Vector3(-5, -1, 9) ,new Vector3(-5, 1, 8), new Vector3(2, 5, 5), new Vector3(2, -5, -5) )
             // Where the particles come from
             particleSystem.emitter = emitterBox; // the starting object, the emitter
-            particleSystem.minEmitBox = new Vector3(-1, -1, 1); // Starting all from
-            particleSystem.maxEmitBox = new Vector3(1, 1, 1); // To...
-
+            particleSystem.minEmitBox = new Vector3(1, -2, 2); // Starting all from
+            particleSystem.maxEmitBox = new Vector3(1, 2, -2); // To...
 
             //Texture of each particle
             particleSystem.particleTexture = new Texture("/textures/flare.png", scene);
 
-        
-
             // Colors of all particles
-            particleSystem.color1 = new Color4(1, 1, 1, 1);
+            particleSystem.color1 = new Color4(.8, .7, 1.5, 2);
             particleSystem.color2 = new Color4(1, 1, 1, 1);
-            particleSystem.colorDead = new Color4(1, 1, 1, 1);
+            particleSystem.colorDead = new Color4(.5, 1, 1, 0);
 
             // Size of each particle (random between...
             particleSystem.minSize = 0.1;
-            particleSystem.maxSize = 1.5;
+            particleSystem.maxSize = 0.2;
 
             // Life time of each particle (random between...
-            particleSystem.minLifeTime = 1.3;
-            particleSystem.maxLifeTime = 2.5;
+            particleSystem.minLifeTime = 2;
+            particleSystem.maxLifeTime = 4;
 
             // Emission rate
-            particleSystem.emitRate = 50;
+            particleSystem.emitRate = 250;
 
             // Blend mode : BLENDMODE_ONEONE, or BLENDMODE_STANDARD
             particleSystem.blendMode = ParticleSystem.BLENDMODE_ADD;
@@ -74,10 +68,9 @@ export const WayfindingRendere = async (canvas: HTMLCanvasElement) => {
             //particleSystem.gravity = new Vector3(0, -9.81, 0);
 
             // Direction of each particle after it has been emitted
-            particleSystem.direction1 = new Vector3(-5, 0, 0);
-            particleSystem.direction2 = new Vector3(-1, 0, 0);
+            particleSystem.direction1 = new Vector3(-1, -.1, 0);
+            particleSystem.direction2 = new Vector3(-1, .1, 0);
         
-
             // Angular speed, in radians
             particleSystem.minAngularSpeed = 0;
             particleSystem.maxAngularSpeed = Math.PI;
@@ -91,6 +84,8 @@ export const WayfindingRendere = async (canvas: HTMLCanvasElement) => {
 
             // Start the particle system
             particleSystem.start();
+
+            emitterBox.rotate(new Vector3(0, 1, 0), 0 * Math.PI / 180 );
         }
 
         
