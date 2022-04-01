5296fa71eccdaedea00d5c08bd5289ee555f9c10
Rasmus Rune Andersen <rasmus@entropyfox.com>
2022-04-01 10:58:44 +0200

    Active camera

--- src/wayfinding/wayfinding.renderer.ts
+++ src/wayfinding/wayfinding.renderer.ts
@@ -1,5 +1,17 @@
-import { AssetsManager, Color4, Engine, MeshBuilder, Quaternion,ParticleSystem, Space, Texture, TransformNode, Vector3, DeviceOrientationCamera } from "@babylonjs/core";
-import { initializeScene } from "../webgl/scene.setup";
+import {
+    AssetsManager,
+    Color4,
+    Engine,
+    MeshBuilder,
+    Quaternion,
+    ParticleSystem,
+    Space,
+    Texture,
+    TransformNode,
+    Vector3,
+    DeviceOrientationCamera,
+} from '@babylonjs/core';
+import { initializeScene } from '../webgl/scene.setup';
 
 export const WayfindingRendere = async (canvas: HTMLCanvasElement) => {
     // Initialize engine and scene
@@ -7,19 +19,21 @@ export const WayfindingRendere = async (canvas: HTMLCanvasElement) => {
     const scene = initializeScene(engine)(canvas);
     // scene.clearColor = new Color4(0, 0, 0, 0);
 
+    /********** DEVICE ORIENTATION CAMERA EXAMPLE **************************/
 
-	/********** DEVICE ORIENTATION CAMERA EXAMPLE **************************/
-
-    // This creates and positions a device orientation camera 	
-    var camera = new DeviceOrientationCamera("DevOr_camera", new Vector3(0, 0, 0), scene);
+    // This creates and positions a device orientation camera
+    var camera = new DeviceOrientationCamera(
+        'DevOr_camera',
+        new Vector3(0, 0, 0),
+        scene
+    );
 
     // This targets the camera to scene origin
     camera.setTarget(new Vector3(0, 0, 10));
 
-
     // This attaches the camera to the canvas
     camera.attachControl(canvas, true);
-	/**************************************************************/
+    scene.setActiveCameraByName('DevOr_camera');
 
     // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
     //var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
@@ -30,24 +44,33 @@ export const WayfindingRendere = async (canvas: HTMLCanvasElement) => {
 
     /// Load arrow
     const assetsManager = new AssetsManager(scene);
-	const meshTask = assetsManager.addMeshTask("Arrow task", "", "https://storage.googleapis.com/hololink/dev/arrow/", "scene.gltf");
+    const meshTask = assetsManager.addMeshTask(
+        'Arrow task',
+        '',
+        'https://storage.googleapis.com/hololink/dev/arrow/',
+        'scene.gltf'
+    );
     meshTask.onSuccess = (task) => {
         console.log('task: ', task);
         // task.loadedMeshes[0].position = new Vector3(0, 0, 1);
         const arrow = task.loadedMeshes[0];
         arrow.parent = content;
-        arrow.rotationQuaternion = Quaternion.FromEulerAngles(0, 0, Math.PI / 2);
+        arrow.rotationQuaternion = Quaternion.FromEulerAngles(
+            0,
+            0,
+            Math.PI / 2
+        );
 
         // Load Particle system
         // We don't have to load the particles in this callback
         loadParticles: {
-            const emitterBox = MeshBuilder.CreateBox("emitter", {size:2})
-            emitterBox.position = new Vector3(2, 0, 0)
-            emitterBox.setPivotPoint(new Vector3(-2,0,0))
-            emitterBox.visibility = 0.4
+            const emitterBox = MeshBuilder.CreateBox('emitter', { size: 2 });
+            emitterBox.position = new Vector3(2, 0, 0);
+            emitterBox.setPivotPoint(new Vector3(-2, 0, 0));
+            emitterBox.visibility = 0.4;
 
             // Create a particle system
-            const particleSystem = new ParticleSystem("particles", 2000, scene);
+            const particleSystem = new ParticleSystem('particles', 2000, scene);
 
             //Particle emitter
             //particleSystem.createBoxEmitter(new Vector3(-5, -1, 9) ,new Vector3(-5, 1, 8), new Vector3(2, 5, 5), new Vector3(2, -5, -5) )
@@ -57,12 +80,15 @@ export const WayfindingRendere = async (canvas: HTMLCanvasElement) => {
             particleSystem.maxEmitBox = new Vector3(1, 2, -2); // To...
 
             //Texture of each particle
-            particleSystem.particleTexture = new Texture("/textures/flare.png", scene);
+            particleSystem.particleTexture = new Texture(
+                '/textures/flare.png',
+                scene
+            );
 
             // Colors of all particles
-            particleSystem.color1 = new Color4(.8, .7, 1.5, 2);
+            particleSystem.color1 = new Color4(0.8, 0.7, 1.5, 2);
             particleSystem.color2 = new Color4(1, 1, 1, 1);
-            particleSystem.colorDead = new Color4(.5, 1, 1, 0);
+            particleSystem.colorDead = new Color4(0.5, 1, 1, 0);
 
             // Size of each particle (random between...
             particleSystem.minSize = 0.1;
@@ -82,9 +108,9 @@ export const WayfindingRendere = async (canvas: HTMLCanvasElement) => {
             //particleSystem.gravity = new Vector3(0, -9.81, 0);
 
             // Direction of each particle after it has been emitted
-            particleSystem.direction1 = new Vector3(-1, -.1, 0);
-            particleSystem.direction2 = new Vector3(-1, .1, 0);
-        
+            particleSystem.direction1 = new Vector3(-1, -0.1, 0);
+            particleSystem.direction2 = new Vector3(-1, 0.1, 0);
+
             // Angular speed, in radians
             particleSystem.minAngularSpeed = 0;
             particleSystem.maxAngularSpeed = Math.PI;
@@ -100,16 +126,15 @@ export const WayfindingRendere = async (canvas: HTMLCanvasElement) => {
             particleSystem.start();
 
             //emitterBox.rotate(new Vector3(0, 1, 0), 0 * Math.PI / 180 );
-            emitterBox.rotationQuaternion = Quaternion.FromEulerAngles(0, 0, Math.PI / 2);
+            emitterBox.rotationQuaternion = Quaternion.FromEulerAngles(
+                0,
+                0,
+                Math.PI / 2
+            );
         }
-
-        
-    }
+    };
 
     assetsManager.load();
-
-  
-   
 
     engine.runRenderLoop(() => {
         scene.render();
@@ -118,9 +143,7 @@ export const WayfindingRendere = async (canvas: HTMLCanvasElement) => {
         engine.resize();
     });
 
-
-
     return {
-        content        
-    }
-}
+        content,
+    };
+};
--- src/wayfinding/wayfinding.ts
+++ src/wayfinding/wayfinding.ts
@@ -1,43 +1,52 @@
-import { Angle, Quaternion } from "@babylonjs/core";
-import { map, mergeMap, tap } from "rxjs/operators";
-import { Compass } from "./compass";
-import { GeoLocation, GeoPoint } from "./geolocation";
-import { Sensors } from "./sensors"
-import { WayfindingRendere } from "./wayfinding.rendere";
+import { Angle, Quaternion } from '@babylonjs/core';
+import { map, mergeMap, tap } from 'rxjs/operators';
+import { Compass } from './compass';
+import { GeoLocation, GeoPoint } from './geolocation';
+import { Sensors } from './sensors';
+import { WayfindingRendere } from './wayfinding.renderer';
 
-export const WayFinding = async() => {
+export const WayFinding = async () => {
     console.log('Way finding');
 
     const skjoldungerne: GeoPoint = {
         lat: 55.59919287227375,
-        lng: 11.978895680155723
+        lng: 11.978895680155723,
     };
 
     const stationen: GeoPoint = {
         lat: 55.60487774953194,
-        lng: 11.971625664068533
-    }
+        lng: 11.971625664068533,
+    };
 
-    const canvas = document.getElementById('renderCanvasWayfinding') as HTMLCanvasElement;
-    const rendere = await WayfindingRendere(canvas);
-    const sensors = Sensors();
+    const canvas = document.getElementById(
+        'renderCanvasWayfinding'
+    ) as HTMLCanvasElement;
+    const renderer = await WayfindingRendere(canvas);
+    //const sensors = Sensors();
     const compass = await Compass();
     const geoLocation = GeoLocation(skjoldungerne);
 
     const updateModel = (angleToPoint: number) => (heading: number) => {
         const angle = Angle.FromDegrees(heading);
-        const modelQuaternion = Quaternion.FromEulerAngles(0, 0, angle.radians() - angleToPoint);
-        rendere.content.rotationQuaternion = modelQuaternion;
-    }
+        const modelQuaternion = Quaternion.FromEulerAngles(
+            0,
+            0,
+            angle.radians() - angleToPoint
+        );
+        renderer.content.rotationQuaternion = modelQuaternion;
+    };
 
-    geoLocation.geolocation$.pipe(
-        map(geoloc => {
-            return {
-                distance: geoloc.distance,
-                angle: Angle.FromDegrees(geoloc.angle).radians()
-            };
-        }),
-        mergeMap(geoloc => {
-            return compass.heading$.pipe(tap(updateModel(geoloc.angle)))
-    })).subscribe();
-}
+    geoLocation.geolocation$
+        .pipe(
+            map((geoloc) => {
+                return {
+                    distance: geoloc.distance,
+                    angle: Angle.FromDegrees(geoloc.angle).radians(),
+                };
+            }),
+            mergeMap((geoloc) => {
+                return compass.heading$.pipe(tap(updateModel(geoloc.angle)));
+            })
+        )
+        .subscribe();
+};
